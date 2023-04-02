c41651cf3dbd63d2a237aeae7a9b411ff2e650e4
Tyler Kemp <ttkemp10@yahoo.com>
2023-04-02 15:12:11 -0400

    created function for writeCards and countDown

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return array;
+}
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0)
+  console.log(countDown--);
+}
